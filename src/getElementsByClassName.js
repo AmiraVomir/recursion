ec89af9d1bd5696755fe854d642998519233af5c
Hieu Ho <hihho@ucdavis.edu>
2020-03-03 16:19:30 -0800

    Add getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {    //Should return arr-like obj of all elements with className;
   // your code here
+  var eleClass = [];
+
+  //find class list > how? > ele.classList;
+  var findClass = function(ele) {
+    //if classlist has className, add > how? > use node.contains;
+    if (ele.classList && ele.classList.contains(className)) {
+      eleClass.push(ele);
+    }
+
+    //if has childnode, recursive into nodes to find class > how? > ele.childnode > func for childnode? > forloop
+    if(ele.hasChildNodes) {
+      let children = ele.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        findClass(children[i]);
+      }
+    }
+  }
+  //recursive search doc.body; > how? > need func for find class > recursive with new func
+  findClass(document.body);
+  //return eleClass;
+  return eleClass;
 };
